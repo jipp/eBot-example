6f7c796d6886bc372ccd64159a17b8db10663a8a
jipp <wolfgang.keller@wobilix.de>
2017-04-19 10:24:57 +0200

    started autnomous mode

--- src/main.cpp
+++ src/main.cpp
@@ -1,70 +1,91 @@
 #include "Arduino.h"
-
 #include <EBot.h>
+#include <Streaming.h>
+
+#define DISTANCE  40
+#define SPEED 120
 
 EBot eBot = EBot();
+unsigned long distance = 0;
+int angle = 0;
+int angleStart = 30;
+int angleStop = 180 - angleStart;
+int angleDelta = 30;
+unsigned long min = DISTANCE;
+unsigned long minM = DISTANCE;
+enum direction { STOP, FORWARD, BACKWARD, TURNLEFT, TURNRIGHT, ROTATELEFT, ROTATERIGHT };
+direction move = STOP;
 
-int ABS = 150;
-int rightDistance = 0,leftDistance = 0,middleDistance = 0 ;
+void drive(direction move) {
+  switch (move) {
+    case STOP:
+    Serial << "STOP" << endl;
+    eBot.stop();
+    break;
+    case FORWARD:
+    Serial << "FORWARD" << endl;
+    eBot.forward(SPEED);
+    break;
+    case BACKWARD:
+    Serial << "BACKWARD" << endl;
+    eBot.backward(SPEED);
+    break;
+    case TURNLEFT:
+    Serial << "TURNLEFT" << endl;
+    eBot.turnLeft(SPEED);
+    break;
+    case TURNRIGHT:
+    Serial << "TURNRIGHT" << endl;
+    eBot.turnRight(SPEED);
+    break;
+    case ROTATELEFT:
+    Serial << "ROTATELEFT" << endl;
+    eBot.rotateLeft(SPEED);
+    break;
+    case ROTATERIGHT:
+    Serial << "ROTATERIGHT" << endl;
+    eBot.rotateRight(SPEED);
+    break;
+  }
+}
 
 void setup() {
   Serial.begin(115200);
-  eBot.move(true);
-  eBot.stop();
+  eBot.begin();
 }
 
 void loop() {
-  eBot.servoRotate(90);
-  delay(500);
-  middleDistance = eBot.distance();
-  #ifdef send
-  Serial.print("middleDistance=");
-  Serial.println(middleDistance);
-  #endif
+  //eBot.write(angle);
+  //delay(10);
+  distance = eBot.distance();
 
-  if(middleDistance<=20) {
-    eBot.stop();
-    delay(500);
-    eBot.servoRotate(5);
-    delay(1000);
-    rightDistance = eBot.distance();
-
-    #ifdef send
-    Serial.print("rightDistance=");
-    Serial.println(rightDistance);
-    #endif
-
-    delay(500);
-    eBot.servoRotate(90);
-    delay(1000);
-    eBot.servoRotate(180);
-    delay(1000);
-    leftDistance = eBot.distance();
-
-    #ifdef send
-    Serial.print("leftDistance=");
-    Serial.println(leftDistance);
-    #endif
-
-    delay(500);
-    eBot.servoRotate(90);
-    delay(1000);
-    if(rightDistance>leftDistance) {
-      eBot.move(true);
-      eBot.barkRight();
-      delay(360);
-    }
-    else if(rightDistance<leftDistance) {
-      eBot.barkLeft();
-      delay(360);
-    }
-    else if((rightDistance<=20)||(leftDistance<=20)) {
-      eBot.backward();
-      delay(180);
-    } else {
-      eBot.forward();
-    }
-  } else {
-    eBot.forward();
+  if (distance < minM && angle == 90) {
+    minM = distance;
+    move = STOP;
   }
+  if (distance > DISTANCE && angle == 90) {
+    minM = DISTANCE;
+    move = FORWARD;
+  }
+
+  if (distance < min && angle < 90) {
+    min = distance;
+    move = TURNRIGHT;
+  }
+
+  if (distance < min && angle > 90) {
+    min = distance;
+    move = TURNLEFT;
+  }
+
+  Serial << "angle=" << angle << " min=" << min << " distance=" << distance << endl;
+  angle += angleDelta;
+  if (angle > angleStop || angle < angleStart) {
+    drive(move);
+    angleDelta = -angleDelta;
+    angle += angleDelta;
+    min = DISTANCE;
+    minM = DISTANCE;
+  }
+  delay(1000);
 }
