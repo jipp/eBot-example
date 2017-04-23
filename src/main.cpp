a65e4991084475ef4e496044e39bca337c96473a
jipp <wolfgang.keller@wobilix.de>
2017-04-23 08:25:36 +0200

    added debug

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,6 @@
-//#define IRREMOTE
-
 #include "Arduino.h"
-#include <EBot.h>
 #include <Streaming.h>
+#include <EBot.h>
 
 #define DISTANCE  40
 
@@ -10,6 +8,13 @@ EBot eBot = EBot();
 unsigned long distance = 0;
 bool manualMode = true;
 int speed = 120;
+int speedDelta = 10;
+volatile int state = LOW;
+
+void stateChange() {
+  state = !state;
+  digitalWrite(LED_BUILTIN, state);
+}
 
 bool checkDistance() {
   if (eBot.getDistance() < DISTANCE) {
@@ -21,9 +26,11 @@ bool checkDistance() {
 void checkCommands(char str) {
   switch (str) {
     case 's':
+    Serial << 's' << endl;
     if (manualMode) eBot.setDirection();
     break;
     case 'f':
+    Serial << 'f' << endl;
     if (manualMode)  {
       if (checkDistance()) {
         eBot.setDirection(EBot::FORWARD);
@@ -31,32 +38,43 @@ void checkCommands(char str) {
     }
     break;
     case 'b':
+    Serial << 'b' << endl;
     if (manualMode) eBot.setDirection(EBot::BACKWARD);
     break;
     case 'l':
+    Serial << 'l' << endl;
     if (manualMode) eBot.setDirection(EBot::TURNLEFT);
     break;
     case 'r':
+    Serial << 'r' << endl;
     if (manualMode) eBot.setDirection(EBot::TURNRIGHT);
     break;
     case 'L':
+    Serial << 'L' << endl;
     if (manualMode) eBot.setDirection(EBot::ROTATELEFT);
     break;
     case 'R':
+    Serial << 'R' << endl;
     if (manualMode) eBot.setDirection(EBot::ROTATERIGHT);
     break;
+    case 'c':
+    Serial << 'c' << endl;
+    stateChange();
+    break;
+    case '-':
+    Serial << '-' << endl;
+    if (manualMode) speed = eBot.setSpeed(--speed);
+    break;
+    case '+':
+    Serial << '+' << endl;
+    speed += speedDelta;
+    if (manualMode) speed = eBot.setSpeed(--speed);
+    break;
     case 'M':
+    Serial << 'M' << endl;
     manualMode = !manualMode;
     break;
   }
-}
-
-char getSerial() {
-  return Serial.read();
-}
-
-char getIR() {
-  return 'n';
 }
 
 void checkObstacle() {
@@ -72,7 +90,6 @@ void setup() {
 }
 
 void loop() {
-  checkCommands(getSerial());
-  // checkCommands(getIR());
+  checkCommands(Serial.read());
   checkObstacle();
 }
